735bc2c26953f28627bf1a12954149182405f183
mcginnessdr <136840228+mcginnessdr@users.noreply.github.com>
2023-06-17 14:21:05 -0400

    start of a new page

---
+++ dans book test game.py
@@ -0,0 +1,16 @@
+import pygame
+import time
+import random
+
+WIDTH, HEIGHT = 1000, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Temp Name") # title of game window
+pygame.font.init()
+FONT = pygame.font.SysFont("impact", 50) # change game font and font size
+BG = pygame.image.load("backgroundimage.jpeg") # change background image
+
+
+def main():
+    
+
+
