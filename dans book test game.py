7f853f2d3d97788acd68771e5f7a0847a9b38b2e
mcginnessdr <136840228+mcginnessdr@users.noreply.github.com>
2023-06-17 19:12:12 -0400

    added classes

--- dans book test game.py
+++ dans book test game.py
@@ -2,15 +2,36 @@ import pygame
 import time
 import random
 
+
 WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Temp Name") # title of game window
+pygame.display.set_caption("Dan's Book - Test Game") # title of game window
 pygame.font.init()
 FONT = pygame.font.SysFont("impact", 50) # change game font and font size
 BG = pygame.image.load("backgroundimage.jpeg") # change background image
 
 
+class Book():
+    def __init__(self):
+        self.title = ""
+        self.sub_title = ""
+        self.author = ""
+        self.open_or_close = 0
+
+class Page():
+    def __init__(self):
+        self.page_num = 0
+        self.page_title = ""
+        self.page_sub_title = ""
+        self.body_text = ""
+        self.left_or_right = 0
+
+class Author():
+    def __init__(self):
+        self.f_name = ""
+        self.l_name = ""
+        self.password = ""
+
+
 def main():
     
-
-
