b6d603f9c5432e5be7bc7ac237e7d2208912a089
mcginnessdr <136840228+mcginnessdr@users.noreply.github.com>
2023-06-19 14:44:43 -0400

    reorganized functions for main menu, fixed classes

--- dans book test game.py
+++ dans book test game.py
@@ -7,20 +7,19 @@ WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Dan's Book - Test Game") # title of game window
 pygame.font.init()
-FONT = pygame.font.SysFont("impact", 30) # change game font and font size
+FONT = pygame.font.SysFont("impact", 25) # change game font and font size
 BG = pygame.image.load("backgroundimage.jpeg") # change background image
 
 
 class Library():
     def __init__(self):
-        self.
+        def ...
 
 class Book():
     def __init__(self):
         self.title = ""
         self.sub_title = ""
         self.author = ""
-        self.open_or_close = 0
 
 class Page():
     def __init__(self):
@@ -28,7 +27,6 @@ class Page():
         self.page_title = ""
         self.page_sub_title = ""
         self.body_text = ""
-        self.left_or_right = 0
 
 class Author():
     def __init__(self):
@@ -62,68 +60,20 @@ def draw():
 
 
 def main_menu():
-    button_one_text = FONT.render("Enter Library", (255, 255, 255))
-    button_two_text = FONT.render("Exit", (255, 255, 255))
-    WIN.blit(button_one_text, (200, 100))
-    WIN.blit(button_two_text, (200, 200))
 
 
-    def exit_game():
-
-    def library():
-
-
-        def book():
-
-        def save_progress():
-
-        def delete_book():
-
-        def publish_book():
-
-
-def author():
-
-    def new_author():
-
-    def edit_author(:)
-
-    def delete_author():
-
-
-def left_page():
-
-    def new_page():
-
-    def edit_page():
-
-    def delete_page():
-
-
-def right_page():
-
-    def new_page():
-
-    def edit_page():
-
-    def delete_page():
-
-
-def title():
-
-    def edit_title():
-
-
-def body():
-
-    def edit_body():
-
-
-def page_num():
-
-    def add_page_num():
+def library():
     
-    def delete_page_num():
+    def create_new_book():
+
+    def edit_book():
+
+    def delete_book():
+
+    
+    
+def exit()
+    
 
 
 if __name__ == "__main__":
