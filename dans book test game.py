d27f7d8d69b709f6b97b2049f7dee391feba49ab
mcginnessdr <136840228+mcginnessdr@users.noreply.github.com>
2023-06-19 23:58:11 -0400

    added controls

--- dans book test game.py
+++ dans book test game.py
@@ -2,7 +2,6 @@ import pygame, os, time, random
 
 
 
-
 class Game():
     def __init__(self):
         pygame.init()
@@ -11,8 +10,8 @@ class Game():
         self.WIN_TITLE = pygame.display.set_caption("Dan's Book - Test Game")
         self.FONT = pygame.font.SysFont("impact", 20)
         self.BG = pygame.image.load("backgroundimage.jpeg")
-        self.CANVAS = pygame.Surface((self.WIN_WIDTH, self.WIN_HEIGHT))
-        #self.running, self.playing = True, True
+        self.CANVAS = pygame.Surface((self.WIN_WIDTH, self.WIN_HEIGHT)) # NEED TO STUDY
+        self.running, self.playing = True, True # NEED TO STUDY
         self.actions = {
             "left" : False,
             "right" : False,
@@ -23,9 +22,54 @@ class Game():
             "back" : False,
             "enter" : False
         }
-        #self.dt, self.prev_time = 0, 0
-        #self.state_stack = []
-        #self.load_assets()
+        self.dt, self.prev_time = 0, 0 # NEED TO STUDY
+        self.state_stack = [] # NEED TO STUDY
+        self.load_assets() 
+
+
+
+    def get_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+                self.running = False
+            if event.type == pygame.KEYDOWN:
+                if event.type == pygame.K_ESCAPE:
+                    self.playing = False
+                    self.running = False
+                if event.key == pygame.K_a:
+                    self.actions["left"] = True
+                if event.key == pygame.K_d:
+                    self.actions["right"] = True
+                if event.key == pygame.K_w:
+                    self.actions["up"] = True
+                if event.key == pygame.K_s:
+                    self.actions["down"] = True
+                if event.key == pygame.K_q:
+                    self.actions["action_1"] = True
+                if event.key == pygame.K_e:
+                    self.actions["action_2"] = True
+                if event.key == pygame.K_BACKSPACE:
+                    self.actions["back"] = True
+                if event.key == pygame.K_RETURN:
+                    self.actions["enter"] = True
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_a:
+                    self.actions["left"] = False
+                if event.key == pygame.K_d:
+                    self.actions["right"] = False
+                if event.key == pygame.K_w:
+                    self.actions["up"] = False
+                if event.key == pygame.K_s:
+                    self.actions["down"] = False
+                if event.key == pygame.K_q:
+                    self.actions["action_1"] = False
+                if event.key == pygame.K_e:
+                    self.actions["action_2"] = False
+                if event.key == pygame.K_BACKSPACE:
+                    self.actions["back"] = False
+                if event.key == pygame.K_RETURN:
+                    self.actions["enter"] = False
 
 
 
@@ -39,13 +83,11 @@ class Library():
 
 
 
-
 class Book():
     def __init__(self):
         self.book_title = ""
         self.book_sub_title = ""
         self.book_author = ""
-
 
 
 
@@ -58,9 +100,9 @@ class Page():
 
 
 
-
 class Author():
     def __init__(self):
         self.f_name = ""
         self.l_name = ""
         self.password = ""
+        
